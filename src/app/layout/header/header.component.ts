f8fab569e61cf6979381e581758f4385f87638c2
Alexandre Kimtsaris <alexandre.kimtsaris@bstorm.be>
2023-10-26 14:59:52 +0200

    demo directive custom

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild} from '@angular/core';
+import {Link} from "../../shared/models/link.model";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit{
+
+  @ViewChild('submenu') submenuList!: ElementRef;
+
+  links: Link[] = [
+    {
+      title: 'home',
+      url: 'home',
+    },
+    {
+      title: 'demo',
+      children: [
+        {
+          title: 'directives',
+          children: [
+            {
+              title: '1',
+              url: '/demo/directive'
+            },
+            {
+              title: '2',
+              url: '/demo/directive'
+            },
+            {
+              title: '3',
+              url: '/demo/directive'
+            },
+          ]
+        },
+        {
+          title: 'directives',
+          children: [
+            {
+              title: '1',
+              url: '/demo/directive'
+            },
+            {
+              title: '2',
+              url: '/demo/directive'
+            },
+            {
+              title: '3',
+              url: '/demo/directive'
+            },
+          ]
+        }
+      ]
+    },
+    {
+      title: 'exo',
+      children: [
+        {
+          title: 'chronomètre(pipe)',
+          url: 'exo/pipe'
+        }
+      ]
+    }
+  ]
+
+  ngAfterViewInit(): void {
+
+  }
+
+
+
 
 }
